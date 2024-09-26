ffc0484d61378fb41c5dda5847846b366ded2963
Quynlys <Lyvanquyen2k4@gmail.com>
2024-09-26 20:40:24 +0700

    Upload shop than lan

---
+++ config.js
@@ -0,0 +1,24 @@
+const sanpham = 
+        [{
+            'imgsrc' : 'https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/460942422_3166337533507752_6520185285960664308_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeGV8ahHiwJuiCgdjeDj2J1zHr2mmZo3sCYevaaZmjewJgXcq8h9aGKwIedOs2Vwzq2cQDwy6fKWnqXN9bh2j58X&_nc_ohc=5teA6cYh37IQ7kNvgEVgxOl&_nc_ht=scontent.fhan14-1.fna&oh=00_AYA5UshZO8uw-GJEFiyjd09hkjfg8EL6s_AKRKUmyD5IGg&oe=66FB1302',
+            'title' : 'Thằn lằn loại premium',
+            'price' : '500.000đ'
+        },
+        {
+            'imgsrc' : 'https://scontent.fhan14-5.fna.fbcdn.net/v/t39.30808-6/456541954_122159004980246387_8522313249322863887_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHGkuhQSeJt31tpP44t920XuIHcrt6zcZK4gdyu3rNxkrPnl_AVMo6JyCge50tkLqm-V13YfC8WBUrCXwWmEXYu&_nc_ohc=NkcBdT-mg2IQ7kNvgHQERzg&_nc_ht=scontent.fhan14-5.fna&oh=00_AYCs4YNCgtkmMQhgtx12PRuoWftaAMrLsIuog7NKSSzX2g&oe=66FB34F6',
+            'title' : 'Thằn lằn loại vip',
+            'price' : '350.000đ'
+        },
+        {
+            'imgsrc' : 'https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/460925040_566062959086895_2509831826705029844_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFt0WwHURHRqCL_Ge8A2D7QQUf71r_GwqNBR_vWv8bCo2C1Wf7IHK3YztSYGVij-gdMCEINg_f7K8GFDHD0GR3u&_nc_ohc=U68Bh_zX2r0Q7kNvgHVKw-K&_nc_ht=scontent.fhan14-2.fna&_nc_gid=AAipxG81dkgfTARoAYzxyU0&oh=03_Q7cD1QHAoxuHSFW85zDD_rcEd575bwK74u87VUowR1D5BELBFA&oe=671CBF35',
+            'title' : 'Thằn lằn loại Thầy ông phương',
+            'price' : '600.000đ'
+        },
+        {
+            'imgsrc' : 'https://scontent.fhan14-4.fna.fbcdn.net/v/t1.15752-9/460943768_919814973378819_7303392679835020571_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeE_kB1dQj4xQ5Ru8xLr0JUGzbQFgrop6K3NtAWCuinorXyMc1T7l9TFnx9cKMoXkbKaHllN8yiSVD-kqmXMmQry&_nc_ohc=yXFPFfSRdeMQ7kNvgGSkHZb&_nc_ht=scontent.fhan14-4.fna&oh=03_Q7cD1QGxJ5ZMY6m5CQzJwbCeYnDGzPcTgzqdLwvzAIjRhAtN0w&oe=671CB247',
+            'title':'Thằn lằn Trần Quyến',
+            'price' : '300.000đ'
+        }
+
+    ];
+    
